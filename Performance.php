f0cca88d82a5948957cad0ede295c4b4fb125c0b
Jamison Lofthouse <jamison.lofthouse@gmail.com>
2014-08-11 17:56:47 +0000

    Customized variables.
    Changed example variable strings to information from Performance. Removed locale files that aren't translated and modified the others.

--- Performance.php
+++ Performance.php
@@ -1,42 +1,41 @@
 <?php
 /**
- * BoilerPlate extension - the thing that needs you.
- *
- * For more info see http://mediawiki.org/wiki/Extension:BoilerPlate
+ * Performance - flexible utility panel for MediaWiki
  *
  * @file
  * @ingroup Extensions
- * @author John Doe, 2014
+ * @author Jamison Lofthouse, 2014
  * @license GNU General Public Licence 2.0 or later
  */
 
 $wgExtensionCredits['other'][] = array(
 	'path' => __FILE__,
-	'name' => 'BoilerPlate',
+	'name' => 'Performance',
 	'author' => array(
-		'John Doe',
+		'Jamison Lofthouse',
 	),
-	'version'  => '0.2.0',
-	'url' => 'https://www.mediawiki.org/wiki/Extension:BoilerPlate',
-	'descriptionmsg' => 'boilerplate-desc',
+	'version'  => '0.0.0 Pre-release',
+	'url' => 'https://www.mediawiki.org/wiki/Manual:Extensions',
+	'descriptionmsg' => 'performance-desc',
 );
 
 /* Setup */
 
 // Register files
-$wgAutoloadClasses['BoilerPlateHooks'] = __DIR__ . '/BoilerPlate.hooks.php';
-$wgAutoloadClasses['SpecialHelloWorld'] = __DIR__ . '/specials/SpecialHelloWorld.php';
-$wgMessagesDirs['BoilerPlate'] = __DIR__ . '/i18n';
-$wgExtensionMessagesFiles['BoilerPlateAlias'] = __DIR__ . '/BoilerPlate.i18n.alias.php';
+$wgAutoloadClasses['PerformanceHooks'] = __DIR__ . '/Performance.hooks.php';
+//$wgAutoloadClasses['SpecialHelloWorld'] = __DIR__ . '/specials/SpecialHelloWorld.php'; // Retained for reference
+$wgMessagesDirs['Performance'] = __DIR__ . '/i18n';
+$wgExtensionMessagesFiles['PerformanceAlias'] = __DIR__ . '/Performance.i18n.alias.php';
 
 // Register hooks
-#$wgHooks['NameOfHook'][] = 'BoilerPlateHooks::onNameOfHook';
+//$wgHooks['NameOfHook'][] = 'PerformanceHooks::onNameOfHook';
 
 // Register special pages
-$wgSpecialPages['HelloWorld'] = 'SpecialHelloWorld';
-$wgSpecialPageGroups['HelloWorld'] = 'other';
+//$wgSpecialPages['HelloWorld'] = 'SpecialHelloWorld';
+//$wgSpecialPageGroups['HelloWorld'] = 'other';
 
 // Register modules
+/*
 $wgResourceModules['ext.BoilerPlate.foo'] = array(
 	'scripts' => array(
 		'modules/ext.BoilerPlate.foo.js',
@@ -52,9 +51,9 @@ $wgResourceModules['ext.BoilerPlate.foo'] = array(
 	'localBasePath' => __DIR__,
 	'remoteExtPath' => 'Performance',
 );
-
+*/
 
 /* Configuration */
 
 // Enable Foo
-#$wgBoilerPlateEnableFoo = true;
+//$wgBoilerPlateEnableFoo = true;
